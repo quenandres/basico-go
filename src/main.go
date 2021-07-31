4e9ebe5a23ac8e8ade39b14bf9d18e8525f15a5f
Jhonatan Mejia <quenandres@gmail.com>
2021-07-30 19:21:25 -0500

    Se inicializan variables y se hacen operaciones basicas. Variables, constantes y zero values7/36

--- src/main.go
+++ src/main.go
@@ -3,7 +3,34 @@ package main
 import "fmt"
 
 func main() {
+	// Declaración de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
 
-	fmt.Println("Hello world")
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
 
+	// Declaración de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+	fmt.Println(base)
+	fmt.Println(altura)
+	fmt.Println(area)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+
+	// Area de un cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area cuadrado:", areaCuadrado)
 }
