f0e0ac2cdd11b9296ac58f988a08a6f885b6f86c
Jhonatan Mejia <quenandres@gmail.com>
2021-08-07 17:21:51 -0500

    Se implementan funciones, con parametros y mas de un retorno. Uso de funciones11/36

--- src/main.go
+++ src/main.go
@@ -2,26 +2,28 @@ package main
 
 import "fmt"
 
-func main() {
-	// Declaración de variables
-	helloMessage := "Hello"
-	worldMessage := "World"
-
-	//Println (Print + salto de linea)
-	fmt.Println(helloMessage, worldMessage)
-	fmt.Println(helloMessage, worldMessage)
-
-	//Printf (Imprime y agrega funcion extra al string que se le inserte)
-	nombre := "Platzi"
-	cursos := 500
-	fmt.Printf("%s tiene más de %d cursos \n", nombre, cursos) // %s para indicar String y %d para indicar int
-	fmt.Printf("%v tiene más de %v cursos \n", nombre, cursos) // %v Si no conocemos el tipo de datos
-
-	// Sprintf
-	message := fmt.Sprintf("%s tiene más de %d cursos", nombre, cursos) // Cualquier comando generado aca, sera asignado a la variable
+func normalFunction(message string) { // Con un solo parametro
 	fmt.Println(message)
+}
 
-	// Tipo de datos
-	fmt.Printf("helloMessage: %T \n", helloMessage)
-	fmt.Printf("cursos: %T", cursos)
+func tripleArgument(a, b int, c string) { // Reciba 3 parametros
+	fmt.Println(a, b, c)
+}
+
+func returnValue(a int) int { // Que retorne una respuesta
+	return a * 2
+}
+
+func doubleReturn(a int) (c, d int) { // Que retorne 2 valores
+	return a, a * 2
+}
+
+func main() {
+	normalFunction("Hola mundo")
+	tripleArgument(1, 2, "Hola")
+	value := returnValue(5)
+	fmt.Println("Value: ", value)
+	value1, _ := doubleReturn(2) // Con el gúion bajo, indicamos que no nos interesa ese valor.
+	fmt.Println("Value1: ", value1)
+	//fmt.Println("Value2: ", value2)
 }
