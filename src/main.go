135fc70a0f4b3bcb162a073a8ca140268e8b30be
Jhonatan Mejia <quenandres@gmail.com>
2021-08-08 14:17:17 -0500

    El poder de los ciclos en Golang: for, for while y for forever13/36

--- src/main.go
+++ src/main.go
@@ -2,28 +2,28 @@ package main
 
 import "fmt"
 
-func normalFunction(message string) { // Con un solo parametro
-	fmt.Println(message)
-}
-
-func tripleArgument(a, b int, c string) { // Reciba 3 parametros
-	fmt.Println(a, b, c)
-}
-
-func returnValue(a int) int { // Que retorne una respuesta
-	return a * 2
-}
-
-func doubleReturn(a int) (c, d int) { // Que retorne 2 valores
-	return a, a * 2
-}
-
 func main() {
-	normalFunction("Hola mundo")
-	tripleArgument(1, 2, "Hola")
-	value := returnValue(5)
-	fmt.Println("Value: ", value)
-	value1, _ := doubleReturn(2) // Con el gúion bajo, indicamos que no nos interesa ese valor.
-	fmt.Println("Value1: ", value1)
-	//fmt.Println("Value2: ", value2)
+	// For condicional
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	//For while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	// For forever
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
+
+	// Rof
+	for i := 10; i >= 0; i-- { //
+		fmt.Println(i)
+	}
 }
