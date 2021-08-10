3cee4002efc82e3c27050728cc8d5966ee5ec70d
Jhonatan Mejia <quenandres@gmail.com>
2021-08-10 18:15:15 -0500

    Definicion de slice y de arrays, agregar elementos a un slice. Arrays y Slices18/36

--- src/main.go
+++ src/main.go
@@ -3,23 +3,28 @@ package main
 import "fmt"
 
 func main() {
+	// Array: Los arrays son inmutables por eso son estaticos
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array)) // len brinda el tamaño del array y cap la capacidad maxima del array
 
-	// Defer: Se podria usar como defer para cerrar la conexion con una bd, cerrar archivo que ya se haya leido.
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
+	// Slice: Muy similar al array pero no se indica la capacidad
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
 
-	// Continue: Se utiliza cuando una condicion dada, para que continue a pesar de un error
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-		if i == 2 {
-			fmt.Println("Es 2")
-			continue
-		}
-		// Break: Cuando se quiere detener la ejecución del codigo
-		if i == 8 {
-			fmt.Println("Break")
-			break
-		}
-	}
+	// Métodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])  // El numero es exclusivo
+	fmt.Println(slice[2:4]) // El primero es inclusivo y el segundo es exclusivo
+	fmt.Println(slice[4:])
 
+	// Append
+	slice = append(slice, 7)
+	fmt.Println(slice)
+
+	// Append nueva lista
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...) // Los tres puntos indican que va a descomponer la lista nueva y agregara los elementos a el slice
+	fmt.Println(slice)
 }
