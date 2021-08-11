ea2384e5c45d620eb87241381f2496981da795fd
Jhonatan Mejia <quenandres@gmail.com>
2021-08-10 20:45:39 -0500

    Se recorren los slices y crea una funcion basica de palindromo Recorrido de Slices con Range19/36

--- src/main.go
+++ src/main.go
@@ -1,30 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindromo(text string) {
+	var textReverse string
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+	}
+
+	if text == textReverse {
+		fmt.Println("Es palindromo")
+	} else {
+		fmt.Println("No es palindromo")
+	}
+}
 
 func main() {
-	// Array: Los arrays son inmutables por eso son estaticos
-	var array [4]int
-	array[0] = 1
-	array[1] = 2
-	fmt.Println(array, len(array), cap(array)) // len brinda el tamaño del array y cap la capacidad maxima del array
+	slice := []string{"Hola", "que", "hace"}
 
-	// Slice: Muy similar al array pero no se indica la capacidad
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, len(slice), cap(slice))
+	for i, valor := range slice {
+		fmt.Println(i, valor)
+	}
 
-	// Métodos en el slice
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])  // El numero es exclusivo
-	fmt.Println(slice[2:4]) // El primero es inclusivo y el segundo es exclusivo
-	fmt.Println(slice[4:])
+	for _, valor := range slice { //Escapamos el indice con el _
+		fmt.Println(valor)
+	}
 
-	// Append
-	slice = append(slice, 7)
-	fmt.Println(slice)
+	str := strings.ToLower("amA")
+	isPalindromo(str)
 
-	// Append nueva lista
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...) // Los tres puntos indican que va a descomponer la lista nueva y agregara los elementos a el slice
-	fmt.Println(slice)
 }
