917ef3b4eeb466debb2dcdefea3c3b02635a1c19
Jhonatan Mejia <quenandres@gmail.com>
2021-08-11 22:03:57 -0500

    Explicacion sencilla de como se utilizan los punteros en GO. Structs y Punteros23/36

--- src/main.go
+++ src/main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
-	pk "curso_golang_platzi/src/mypackage" //Alias a la izquierda con un espacio.
 	"fmt"
 )
 
-func main() {
-	// Se indica si una variable, funcion o tipo de datos puede tener acceso publico o privado
-	// Si la primera letra esta en mayuscula significa que es publico, de lo contrario es privado.
-	var myCar pk.CarPublic
-	myCar.Brand = "Ferrari"
-	myCar.Year = 2020
-	//Go nos indica que no podemos acceder a esa variable por no ser metodo publico.
-	fmt.Println(myCar)
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
 
-	//var myOtherCar pk.CarPrivate
-	pk.PrintMessage("Hola Platzi")
-	pk.printMessage("Hola")
+func (myPC pc) ping() {
+	fmt.Println(myPC.brand, "Pong")
+}
+
+func (myPC *pc) duplicateRAM() { //Antes del nombre de la funcion se agrega el puntero
+	myPC.ram = myPC.ram * 2
+}
+
+func main() {
+	a := 50
+	b := &a //Forma de acceder a la dirección en la memoria
+
+	fmt.Println(a)  // Variable
+	fmt.Println(b)  // Direccion de memoria
+	fmt.Println(*b) // Valor que se ubica en esa direccion de memoria
+
+	*b = 100
+	fmt.Println(a) // El valor de a cambia por que se cambio el valor ubicado en esa dirección de memoria
+
+	// Para que se utilizan los punteros: Un ejemplo es para utilizar funciones de una libreria a otro paquete de forma eficiente
+
+	// ej:
+	myPC := pc{ram: 16, disk: 200, brand: "msi"}
+	fmt.Println(myPC)
+	myPC.ping()
+	fmt.Println(myPC)
+	myPC.duplicateRAM()
+	fmt.Println(myPC)
+	myPC.duplicateRAM()
+	fmt.Println(myPC)
+
+	// El simbolo & accede a la memoria de la variable
+	// El simbolo * accede al valor que esta guardado en memoria, usando la dirección de memoria de la variable.
 }
