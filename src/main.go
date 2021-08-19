46b7620468d2629a0c81ff6bd369d76c1f946056
Jhonatan Mejia <quenandres@gmail.com>
2021-08-18 19:58:58 -0500

    Forma basica de modificar modulos con Go.

--- src/main.go
+++ src/main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
+	"fmt"
 )
 
 func main() {
 	// go get para manejo de paquete de terceros.
 	// Instanciar echo
-	e := echo.New()
+	/* e := echo.New()
 
 	// Ruta
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World from GO")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":1323")) */
+	fmt.Println("Hello World")
 }
